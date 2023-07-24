a50e740fde42115f350c32b1c9ce4a04160ce066
Huang-Yuanhang <140174372+Huang-Yuanhang@users.noreply.github.com>
2023-07-24 17:34:11 +0800

    Update score.py

--- score/score.py
+++ score/score.py
@@ -1,11 +1,5 @@
 import random
 
-message = """
-Program to calculate and display a user's bonus based on sales.
-If sales are under $1,000, the user gets a 10% bonus.
-If sales are $1,000 or over, the bonus is 15%.
-"""
-
 MINIMUM_SCORE = 0
 MAXIMUM_SCORE = 100
 EXCELLENT_SCORE = 90
@@ -15,9 +9,11 @@ PASSABLE_SCORE = 50
 def get_score_result(score):
     if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
         return "Invalid score"
-    elif score >= EXCELLENT_SCORE:
+    elif score > MAXIMUM_SCORE:
+        return "Invalid score"
+    elif score > EXCELLENT_SCORE:
         return "Excellent"
-    elif score >= PASSABLE_SCORE:
+    elif score > PASSABLE_SCORE:
         return "Passable"
     else:
         return "Bad"
@@ -28,7 +24,6 @@ def random_score():
 
 
 def main():
-    print(message)
     user_score = float(input("Enter your score: "))
     result = get_score_result(user_score)
     print(result)
@@ -39,3 +34,4 @@ def main():
 
 
 main()
+
